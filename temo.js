cc4f5b13ddc743dad57c198597fd1eff06353bb0
temo087 <lizii7724@gmail.com>
2023-10-28 22:45:39 +0400

    new commit

---
+++ temo.js
@@ -0,0 +1,9 @@
+
+
+
+const brg = document.getElementById("square");
+const brg1 = document.querySelector('.menu'); 
+
+brg.addEventListener('click', function() {
+    brg1.classList.toggle('active'); 
+});
